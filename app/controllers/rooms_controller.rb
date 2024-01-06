b5ff521a9b3283b195d6a70744cec6ac35809825
SH049 <146598693+SH049@users.noreply.github.com>
2024-01-06 15:09:36 +0900

    room作成機能

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,18 +8,42 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.find(params[:id])
+    @room = Room.new(room_params)
+    if @room.save
+      flash[:notice] = "施設を登録しました"
+      redirect_to rooms_path
+    else
+      render "new"
+    end
   end
 
   def show
+    @room = Room.find(params[:id])
   end
 
   def edit
+    @room = Room.find(params[:id])
   end
 
   def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      flash[:notice] = "アカウントを編集しました"
+      redirect_to rooms_path
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    flash[:notice] = "施設を削除しました"
+    redirect_to rooms_path
+  end
+  private
+
+  def room_params
+    params.require(:room).permit(:room_name, :room_detail, :room_fee, :room_address, :room_image)
   end
 end
