4b5d6fba0ad1a3bf61306e548b2866b4395aa2eb
SH049 <146598693+SH049@users.noreply.github.com>
2024-01-17 20:20:46 +0900

    数字のバリデーション（0以上）と三桁区切り

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
     validates :checkin, presence:true
     validates :checkout, presence:true
-    validates :people, presence:true
+    validates :people, presence:true, numericality:{greater_than_or_equal_to: 1}
     mount_uploader :room_image, ImageUploader
     belongs_to :user
     belongs_to :room
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,8 @@
 class Room < ApplicationRecord
-    validates :room_name, presence:true
-    validates :room_detail, presence:true
-    validates :room_fee, presence:true
-    validates :room_address, presence:true
+    validates :room_name, presence: true
+    validates :room_detail, presence: true
+    validates :room_fee, presence: true, numericality:{greater_than_or_equal_to: 1}
+    validates :room_address, presence: true
     mount_uploader :room_image, ImageUploader
     has_many :reservations
     belongs_to :user
