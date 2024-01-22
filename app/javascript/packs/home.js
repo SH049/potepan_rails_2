bb740e95f78c219b43aa866236b6b5cb8ba293e3
SH049 <146598693+SH049@users.noreply.github.com>
2024-01-22 22:01:36 +0900

    予約一覧JS（途中）

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,6 +1,8 @@
 document.addEventListener('turbolinks:load', function() {
     const menuToggle = document.getElementById('menuToggle');
     const menu = document.getElementById('menu');
+    const editDotToggle = document.getElementById('editDotToggle');
+    const editDot = document.getElementById('editDot');
 
     menuToggle.addEventListener('click', function() {
         menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'block' : 'none';
@@ -9,6 +11,16 @@ document.addEventListener('turbolinks:load', function() {
     document.addEventListener('click', (e) => {
         if (!e.target.closest('#menuToggle')) {
             menu.style.display = 'none';
+        }
+    });
+
+    editDotToggle.addEventListener('click', function() {
+        editDot.style.display = (editDot.style.display === 'none' || editDot.style.display === '') ? 'block' : 'none';
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('#editDotToggle')) {
+            editDot.style.display = 'none';
         }
     });
 });
