7464e0a37431a17c209d1a8ba219ed422f9325d1
SH049 <146598693+SH049@users.noreply.github.com>
2024-01-23 21:03:24 +0900

    自分が作成した一覧画面js作成

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,8 +1,8 @@
 document.addEventListener('turbolinks:load', function() {
     const menuToggle = document.getElementById('menuToggle');
     const menu = document.getElementById('menu');
-    const editDotToggle = document.getElementById('editDotToggle');
-    const editDot = document.getElementById('editDot');
+    const editDotToggles = document.querySelectorAll('.editDotToggle');
+    
 
     menuToggle.addEventListener('click', function() {
         menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'block' : 'none';
@@ -14,13 +14,30 @@ document.addEventListener('turbolinks:load', function() {
         }
     });
 
-    editDotToggle.addEventListener('click', function() {
-        editDot.style.display = (editDot.style.display === 'none' || editDot.style.display === '') ? 'block' : 'none';
-    });
+    editDotToggles.forEach(function(editDotToggle) {
+        const reservationId = editDotToggle.dataset.reservationId;
+        const roomId = editDotToggle.dataset.roomId;
+        const editDotReservation = document.getElementById(`editDotReservation${reservationId}`);
+        const editDotRoom = document.getElementById(`editDotRoom${roomId}`);
 
-    document.addEventListener('click', (e) => {
-        if (!e.target.closest('#editDotToggle')) {
-            editDot.style.display = 'none';
-        }
+        editDotToggle.addEventListener('click', function() {
+            editDotReservation.style.display = (editDotReservation.style.display === 'none' || editDotReservation.style.display === '') ? 'block' : 'none';
+        });
+
+        document.addEventListener('click', (e) => {
+            if (!e.target.closest(`.editDotToggle[data-reservation-id="${reservationId}"]`)) {
+                editDotReservation.style.display = 'none';
+            }
+        });
+
+        editDotToggle.addEventListener('click', function() {
+            editDotRoom.style.display = (editDotRoom.style.display === 'none' || editDotRoom.style.display === '') ? 'block' : 'none';
+        });
+
+        document.addEventListener('click', (e) => {
+            if (!e.target.closest(`.editDotToggle[data-room-id="${roomId}"]`)) {
+                editDotRoom.style.display = 'none';
+            }
+        });
     });
 });
